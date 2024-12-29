eb251118efbf1b6a2003cf3d5a1f126ceb2f7d8e
Ankita Sinha <ankitasinha322@gmail.com>
2024-12-29 15:17:06 +0000

    more handler changes

--- main.go
+++ main.go
@@ -5,15 +5,29 @@ import (
 	"net/http"
 )
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+    // Write the Content-Type header
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+    // Write the status code
+    w.WriteHeader(http.StatusOK)
+
+    // Write the body text
+    w.Write([]byte("OK"))
+
+}
+
 func main() {
 
     newMux := http.NewServeMux()
 
+    newMux.HandleFunc("/healthz", readinessHandler)
+
     fileServer := http.FileServer(http.Dir("."))
 
-    newMux.Handle("/", fileServer)
+    newMux.Handle("/app/", http.StripPrefix("/app", fileServer))
 
-    server := &http.Server{
+    server := &http.Server {
         Addr:    ":8080",  // Set the address to listen on port 8080
         Handler: newMux,      // Use the ServeMux as the server's handler
     }
